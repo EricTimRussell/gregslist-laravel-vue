e4cdad24e9d5549696535c80493e074cf0a16e68
Eric Russell <Erictim.Russell@live.com>
2023-08-24 08:34:16 -0600

    php routing and vue component rendering

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Index
+    <Link href="/hello">Hello Page</Link>
+  </div>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Show
+    <Link href="/">Home</Link>
+  </div>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+</script>
+
+<style lang="scss" scoped></style>
