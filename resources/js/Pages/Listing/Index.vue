6ea599f58a02c0f6d9eff155a8c8f77d31a44ef0
Eric Russell <Erictim.Russell@live.com>
2023-08-29 17:30:34 -0600

    frontend links and display data

---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-for="listing in listings" :key="listing.id">
+    <Link :href="`listing/${listing.id}`">
+    {{ listing.city }} {{ listing.street_nr }}, {{ listing.city }}, for {{ listing.price }}
+    </Link>
+  </div>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+defineProps({
+  listings: { type: Array, required: true }
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ resources/js/Pages/Listing/Show.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ listings.city }} {{ listings.street_nr }}, {{ listings.city }}, for {{ listings.price }}
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  listings: { type: Object, required: true }
+})
+</script>
+
+<style lang="scss" scoped></style>
