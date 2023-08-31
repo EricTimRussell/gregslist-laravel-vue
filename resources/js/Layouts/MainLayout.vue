19750907ec2cffe056e5ec88eded41ab71fbe22a
Eric Russell <Erictim.Russell@live.com>
2023-08-31 13:18:38 -0600

    header and flash message styling

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,25 +1,37 @@
 <template>
-  <Link href="/listing">Listings </Link>
-  <Link href="/listing/create">New Listing</Link>
-  <div v-if="flashSuccess" class="success">
-    {{ flashSuccess }}
-  </div>
-  <slot></slot>
+  <header class="border-b border-gray-200 dark:border-gray-700 bg-gray-50 dark:bg-slate-700 w-full">
+    <div class="container mx-auto">
+      <nav class="p-4 flex items-center justify-between">
+        <div class="text-lg font-medium">
+          <Link href="/listing">Listings</Link>
+        </div>
+        <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+          <Link href="/listing">GregsList</Link>
+        </div>
+        <div>
+          <Link href="/listing/create"
+            class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md shadow-lg">+
+          New Listing
+          </Link>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <main class="container mx-auto p-4">
+    <div v-if="flashSuccess"
+      class="mb-4 border rounded-md  shadow-sm border-green-300 dark:border-green-800 bg-green-100 dark:bg-green-900 p-2">
+      {{ flashSuccess }}
+    </div>
+    <slot></slot>
+  </main>
 </template>
 
 <script setup>
 import { Link, usePage } from "@inertiajs/vue3"
 import { computed } from 'vue'
-// page.props.value.flash.success
 const page = usePage()
 const flashSuccess = computed(() =>
   page.props.flash.success
 )
 </script>
-
-<style scoped>
-.success {
-  background-color: green;
-  color: white;
-}
-</style>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,7 @@
     @inertiaHead
 </head>
 
-<body class="bg-white">
+<body class="bg-white dark:bg-slate-900 text-gray-800 dark:text-gray-300">
     @inertia
 </body>
 
