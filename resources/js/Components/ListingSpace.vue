aba915f202f1ca3eafe17d8adf8a32e4b24eab4a
Eric Russell <Erictim.Russell@live.com>
2023-08-31 13:54:48 -0600

    non-prop attr & styling text in box comp

---
+++ resources/js/Components/ListingSpace.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <span class="font-bold">{{ listing.beds }} bds </span><span class="text-gray-400">|</span> <span class="font-bold">{{
+      listing.baths }} ba </span>
+    <span class="text-gray-400">|</span> <span class="font-bold">{{ listing.area }} ft²</span>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  listing: { type: Object, required: true }
+})
+</script>
