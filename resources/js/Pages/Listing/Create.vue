20e8c04d552f4da7a137b56e560ef34e5ba1b11f
Eric Russell <Erictim.Russell@live.com>
2023-09-01 16:14:53 -0600

    disable css autoformat style error msgs

--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -4,14 +4,14 @@
       <div class="col-span-2">
         <label class="label">Beds</label>
         <input v-model.number="form.beds" type="text" class="input" />
-        <div v-if="form.errors.beds">{{ form.errors.beds }}</div>
+        <div class="input-error" v-if="form.errors.beds">{{ form.errors.beds }}</div>
       </div>
 
       <div class="col-span-2">
         <label class="label">Baths</label>
         <input class="input"
           v-model.number="form.baths" type="text" />
-        <div v-if="form.errors.baths">{{ form.errors.baths }}</div>
+        <div class="input-error" v-if="form.errors.baths">{{ form.errors.baths }}</div>
 
       </div>
 
@@ -19,42 +19,42 @@
         <label class="label">Area</label>
         <input class="input"
           v-model.number="form.area" type="text" />
-        <div v-if="form.errors.area">{{ form.errors.area }}</div>
+        <div class="input-error" v-if="form.errors.area">{{ form.errors.area }}</div>
       </div>
 
       <div class="col-span-4">
         <label class="label">City</label>
         <input class="input"
           v-model="form.city" type="text" />
-        <div v-if="form.errors.city">{{ form.errors.city }}</div>
+        <div class="input-error" v-if="form.errors.city">{{ form.errors.city }}</div>
       </div>
 
       <div class="col-span-2">
         <label class="label">Post Code</label>
         <input class="input"
           v-model="form.code" type="text" />
-        <div v-if="form.errors.code">{{ form.errors.code }}</div>
+        <div class="input-error" v-if="form.errors.code">{{ form.errors.code }}</div>
       </div>
 
       <div class="col-span-4">
         <label class="label">Street</label>
         <input class="input"
           v-model="form.street" type="text" />
-        <div v-if="form.errors.street">{{ form.errors.street }}</div>
+        <div class="input-error" v-if="form.errors.street">{{ form.errors.street }}</div>
       </div>
 
       <div class="col-span-2">
         <label class="label">Street #</label>
         <input class="input"
           v-model.number="form.street_nr" type="text" />
-        <div v-if="form.errors.street_nr">{{ form.errors.street_nr }}</div>
+        <div class="input-error" v-if="form.errors.street_nr">{{ form.errors.street_nr }}</div>
       </div>
 
       <div class="col-span-6">
         <label class="label">Price</label>
         <input class="input"
           v-model.number="form.price" type="text" />
-        <div v-if="form.errors.price">{{ form.errors.price }}</div>
+        <div class="input-error" v-if="form.errors.price">{{ form.errors.price }}</div>
       </div>
 
       <div class="col-span-6">
