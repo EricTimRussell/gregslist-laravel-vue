1576ca6ccc7d6a3f815123312ee4be61c5ae7600
Eric Russell <Erictim.Russell@live.com>
2023-09-08 12:10:05 -0600

    protect certain routes with auth

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [IndexController::class, 'index']);
-Route::get('/hello', [IndexController::class, 'show']);
+Route::get('/hello', [IndexController::class, 'show'])->middleware('auth');
 
-Route::resource('listing', ListingController::class);
+
+Route::resource('listing', ListingController::class)
+  // have to be authenticated to visit these routes
+  ->only(['create', 'store', 'edit,', 'update', 'destroy'])->middleware('auth');
+
+Route::resource('listing', ListingController::class)
+  // give access to all routes except these if not authenticated
+  ->except(['create', 'store', 'edit', 'update', 'destroy']);
 
 Route::get('login', [AuthController::class, 'create'])->name('login');
 Route::post('login', [AuthController::class, 'store'])->name('login.store');
