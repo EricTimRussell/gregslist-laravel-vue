a5f1c5fcc7bd14b8c49b3c268b15b644696a35bb
Eric Russell <Erictim.Russell@live.com>
2023-09-11 14:01:26 -0600

    make hash password a model mutator

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -9,22 +9,21 @@ use Illuminate\Support\Facades\Hash;
 
 class UserAccountController extends Controller
 {
+    // Register a user
     public function create()
     {
         return inertia('UserAccount/Create');
     }
-
+    // Save and validate the registered user to the db
     public function store(Request $request)
     {
-        $user = User::make($request->validate([
+        $user = User::create($request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:8|confirmed'
         ]));
-        $user->password = Hash::make($user->password);
-        $user->save();
         Auth::login($user);
-
+        // TODO Change to home page rather than listing page
         return redirect()->route('listing.index')
             ->with('success', 'Account Created');
     }
