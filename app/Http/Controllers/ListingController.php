725004a3fea8c2f6c3a97653726807e749ea8414
Eric Russell <Erictim.Russell@live.com>
2023-09-12 13:24:12 -0600

    eloquent model filter

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -28,32 +28,10 @@ class ListingController extends Controller
             [
                 'filters' => $filters,
                 // using pagination changes it from an array to an array of objects
-                'listings' => Listing::orderByDesc('created_at')
-                    ->when(
-                        // ?? false is the default value if no value is present
-                        $filters['priceFrom'] ?? false,
-                        fn ($query, $value) => $query->where('price', '>=', $value)
-                    )
-                    ->when(
-                        $filters['priceTo'] ?? false,
-                        fn ($query, $value) => $query->where('price', '<=', $value)
-                    )
-                    ->when(
-                        $filters['beds'] ?? false,
-                        fn ($query, $value) => $query->where('beds', (int)$value < 6 ? '=' : '>=', $value)
-                    )
-                    ->when(
-                        $filters['baths'] ?? false,
-                        fn ($query, $value) => $query->where('baths', (int)$value < 6 ? '=' : '>=', $value)
-                    )
-                    ->when(
-                        $filters['areaFrom'] ?? false,
-                        fn ($query, $value) => $query->where('area', '>=', $value)
-                    )
-                    ->when(
-                        $filters['areaTo'] ?? false,
-                        fn ($query, $value) => $query->where('area', '<=', $value)
-                    )->paginate(10)->withQueryString()
+                'listings' => Listing::mostRecent()
+                    ->filter($filters)
+                    ->paginate(10)
+                    ->withQueryString()
             ]
         );
     }
