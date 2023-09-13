4e43c20f2c18432c70fc92121d70f02937c537a1
Eric Russell <Erictim.Russell@live.com>
2023-09-13 13:58:10 -0600

    user account styling and import listings by id

--- resources/js/Pages/Account/Index.vue
+++ resources/js/Pages/Account/Index.vue
@@ -7,8 +7,18 @@
     <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
       <Box v-for="listing in listings" :key="listing.id">
         <div class="flex flex-col md:flex-row gap-2 md:items-center justify-between">
-          <div>Left</div>
-          <div>Right</div>
+          <div>
+            <div class="xl:flex items-center gap-2">
+              <Price :price="listing.price" class="text-2xl font-medium" />
+              <ListingSpace :listing="listing" />
+            </div>
+            <ListingAddress :listing="listing" class="text-gray-500" />
+          </div>
+          <div class="flex items-center gap-1 text-gray-600 dark:text-gray-300">
+            <Link class="btn-outline text-xs font-medium">Preview</Link>
+            <Link class="btn-outline text-xs font-medium">Edit</Link>
+            <Link class="btn-outline text-xs font-medium">Delete</Link>
+          </div>
         </div>
       </Box>
     </section>
@@ -16,8 +26,12 @@
 </template>
 
 <script setup>
-// @ts-ignore
 import Box from '@/Components/UI/Box.vue'
+import Price from '@/Components/Price.vue'
+import ListingSpace from '@/Components/ListingSpace.vue'
+import ListingAddress from '@/Components/ListingAddress.vue'
+import { Link } from "@inertiajs/vue3"
+
 
 defineProps({ listings: { type: Object, required: true } })
 </script>
