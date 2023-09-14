58df1b851efa5eb15505d899444c75829ed0b4b8
Eric Russell <Erictim.Russell@live.com>
2023-09-14 13:23:17 -0600

    item filter component and some styling for it

--- resources/js/Pages/Account/Index.vue
+++ resources/js/Pages/Account/Index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h1 class="text-3xl mb-4">Your Listings</h1>
-    <section class="mb-4">
-      Filters
+    <section>
+      <ItemsFilter />
     </section>
     <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
       <Box v-for="listing in listings" :key="listing.id">
@@ -32,6 +32,7 @@ import Box from '@/Components/UI/Box.vue'
 import Price from '@/Components/Price.vue'
 import ListingSpace from '@/Components/ListingSpace.vue'
 import ListingAddress from '@/Components/ListingAddress.vue'
+import ItemsFilter from '../Account/Index/Components/ItemsFilter.vue'
 import { Link } from "@inertiajs/vue3"
 
 
