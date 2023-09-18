ef82a22266c08e5702cec4e1121199573d019129
Eric Russell <Erictim.Russell@live.com>
2023-09-18 15:41:06 -0600

    backend logic to upload images

--- resources/js/Pages/Listing/ListingImage/Create.vue
+++ resources/js/Pages/Listing/ListingImage/Create.vue
@@ -1,6 +1,6 @@
 <template>
-  <form method="POST" :action="route('listing.image.store', { listing: listing?.id })">
-    <input type="file" multiple name="files" />
+  <form enctype="multipart/form-data" method="POST" :action="route('listing.image.store', { listing: listing?.id })">
+    <input type="file" multiple name="images[]" />
     <button type="submit">Submit</button>
   </form>
 </template>
