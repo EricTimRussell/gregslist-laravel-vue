1e6d2d506a37e07d59c87e8c94c4efc968f671ba
Eric Russell <Erictim.Russell@live.com>
2023-09-18 15:55:14 -0600

    frontend logic to upload files

--- resources/js/Pages/Listing/ListingImage/Create.vue
+++ resources/js/Pages/Listing/ListingImage/Create.vue
@@ -1,14 +1,38 @@
 <template>
-  <form enctype="multipart/form-data" method="POST" :action="route('listing.image.store', { listing: listing?.id })">
-    <input type="file" multiple name="images[]" />
-    <button type="submit">Submit</button>
-  </form>
+  <Box>
+    <template #header>Upload New Images</template>
+    <form @submit.prevent="upload" enctype="multipart/form-data">
+      <input type="file" multiple @input="addFiles" />
+      <button class="btn-outline" type="submit">Upload</button>
+      <button class="btn-outline" type="reset" @click="reset">Reset</button>
+    </form>
+  </Box>
 </template>
 
 <script setup lang="">
+import Box from '@/Components/UI/Box.vue'
+import { useForm } from '@inertiajs/vue3'
 
 const props = defineProps({
   listing: { type: Object, required: false }
 })
+
+const form = useForm({
+  images: [],
+})
+const upload = () => {
+  form.post(
+    route('listing.image.store', { listing: props.listing.id }),
+    {
+      onSuccess: () => form.reset('images'),
+    },
+  )
+}
+const addFiles = (event) => {
+  for (const image of event.target.files) {
+    form.images.push(image)
+  }
+}
+const reset = () => form.reset('images')
 </script>
 
